fe4ee2f02fe81553fa4847fa01ea78867a802d8a
CarolinaCheng616 <948005247@qq.com>
2021-08-02 16:28:47 +0800

    modify

---
+++ draw/drawStatistics.py
@@ -0,0 +1,62 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def read_num_of_event_cate():
+    train_arr = []
+    val_arr = []
+    test_arr = []
+    total_arr = []
+    with open("../data/num_of_event_cate.txt", 'r', encoding='utf-8') as f:
+        for i, line in enumerate(f):
+            if i == 0:
+                continue
+            cate, num = line.strip().split("\t")
+            train, val, test, total = num.split(",")
+            train_arr.append(train)
+            val_arr.append(val)
+            test_arr.append(test)
+            total_arr.append(total)
+    train_arr = np.array(train_arr).reshape(-1, 1)
+    val_arr = np.array(val_arr).reshape(-1, 1)
+    test_arr = np.array(test_arr).reshape(-1, 1)
+    total_arr = np.array(total_arr).reshape(-1, 1)
+    return np.concatenate((train_arr, val_arr, test_arr, total_arr), axis=1), \
+           np.array(["train", "val", "test", "total"])
+
+
+def read_categories():
+    categories = []
+    with open("../data/categories.txt", "r", encoding='utf-8') as f:
+        for line in f:
+            cate = line.strip().split("\t")[0]
+            categories.append(cate)
+    return np.array(categories)
+
+
+def draw_histogram_by_cat(label, number, names, show_name_per_num=1):
+    # number是二维数组，len(number)=len(label)，分组显示，legend和names个数是数组的个数
+    assert len(label) == len(number), 'unequal label and number'
+    length = len(label)
+    fig = plt.figure()
+    label = [cat if i % show_name_per_num == 0 else '' for i, cat in enumerate(label)]
+    groups = number.shape[1]  # 4
+    ew = 0.8 / groups
+    x = np.arange(length)
+    for g in range(groups):  # 总宽度0.8
+        plt.bar(x + ew * g, number[:, g], ew)
+    plt.xticks(np.arange(length),
+               label,
+               fontsize=10,
+               fontweight='bold')
+    fig.autofmt_xdate(rotation=45)
+    fig.tight_layout()
+    plt.grid(axis='y', zorder=1)
+    plt.legend(names)
+    plt.show()
+
+
+if __name__ == '__main__':
+    array, names = read_num_of_event_cate()
+    label = read_categories()
+    draw_histogram_by_cat(label, array, names)
--- utils/statistics.py
+++ utils/statistics.py
@@ -5,8 +5,7 @@ import pandas as pd
 # data/categories.txt
 
 
-def number_of_event_categories():
-    # 训练集、验证集、测试集、以及整体每种事件分别有多少个视频
+def read_categories():
     categories = []
     cate2idx = dict()
     with open("../data/categories.txt", "r", encoding='utf-8') as f:
@@ -15,6 +14,12 @@ def number_of_event_categories():
             categories.append(cate)
             cate2idx[cate] = len(cate2idx)
     categories = np.array(categories)
+    return categories, cate2idx
+
+
+def number_of_event_categories():
+    # 训练集、验证集、测试集、以及整体每种事件分别有多少个视频
+    categories, cate2idx = read_categories()
 
     train = pd.read_csv("../data/AVVP_train.csv", header=0, sep='\t')
     val = pd.read_csv("../data/AVVP_val_pd.csv", header=0, sep='\t')
@@ -50,5 +55,29 @@ def number_of_event_categories():
         f.write("\n".join(statics))
 
 
+def number_of_event_categories_for_modality():
+    # 验证集+测试集每个模态对于每种事件发生的次数
+    categories, cate2idx = read_categories()
+    audio_labels = pd.read_csv("../data/AVVP_eval_audio.csv", header=0, sep='\t')["event_labels"]
+    visual_labels = pd.read_csv("../data/AVVP_eval_visual.csv", header=0, sep='\t')["event_labels"]
+
+    audio_nums = np.zeros(25, dtype=np.int)
+    visual_nums = np.zeros(25, dtype=np.int)
+
+    for i in range(len(audio_labels)):
+        label = audio_labels[i]
+        audio_nums[cate2idx[label]] += 1
+
+    for i in range(len(visual_labels)):
+        label = visual_labels[i]
+        visual_nums[cate2idx[label]] += 1
+
+    statics = ["\t".join([c, ",".join([str(au), str(vi), str(au + vi)])])
+               for c, au, vi in zip(categories, audio_nums, visual_nums)]
+    with open("../data/num_of_event_cate_for_modal.txt", "w", encoding="utf-8") as f:
+        f.write("category\taudio,visual\n")
+        f.write("\n".join(statics))
+
+
 if __name__ == '__main__':
-    number_of_event_categories()
+    number_of_event_categories_for_modality()
